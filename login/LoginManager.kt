fdcb5eb26488402821058b11e74d7c5971a0ff5c
Engine Bai <enginebai@gmail.com>
2022-03-20 15:01:03 +0800

    Add member model

--- login/LoginManager.kt
+++ login/LoginManager.kt
@@ -2,6 +2,7 @@ object LoginManager {
     private val loginApi by inject<LoginApiServicie>()
 
     suspend fun loginByEmail(mail: String, password: String) {
+        // FIXME: exchange the parameters
         loginApi.emailLogin(password, mail)
     }
 }
