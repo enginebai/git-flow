1056fd7b81bc1fd707ed1ff5a76f5dfbfe497f67
Engine Bai <enginebai@gmail.com>
2022-03-20 14:47:40 +0800

    Feature/login (#1)
    * Add login manager
    
    * Add login result
    
    * Add login api service

---
+++ login/LoginManager.kt
@@ -0,0 +1,7 @@
+object LoginManager {
+    private val loginApi by inject<LoginApiServicie>()
+
+    suspend fun loginByEmail(mail: String, password: String) {
+        loginApi.emailLogin(password, mail)
+    }
+}
