c3cec97183898358cc49bf48581dbed868c15d02
enginebai <enginebai@gmail.com>
2022-03-23 14:50:55 +0800

    Fix login API call site

--- login/LoginManager.kt
+++ login/LoginManager.kt
@@ -2,6 +2,12 @@ object LoginManager {
     private val loginApi by inject<LoginApiServicie>()
 
     suspend fun loginByEmail(mail: String, password: String) {
+<<<<<<< HEAD
         loginApi.emailLogin(mail, password)
+=======
+        loading = true
+        loginApi.emailLogin(mail, password).subscribe()
+        loading = false
+>>>>>>> 7d87c3d (Fix login API call site)
     }
 }
